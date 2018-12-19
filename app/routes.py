1c2933b09c7d6b9fbc272713b2d8f1ead17eb337
Salvatore Tosti <saltosti@gmail.com>
2018-12-18 20:10:21 -0800

    Started basic index page

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/board')
+def board():
+    return render_template('board.html')
---
+++ app/static/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#start-button').click(function() {
+        console.log('start')
+	})
+})
+
+$(function(){
+	$('#join-button').click(function() {
+        console.log('join')
+	})
+})
