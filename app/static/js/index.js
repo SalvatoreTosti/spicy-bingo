0c6ec2e945f56e2837365d4ad1bd128cfa13190d
Salvatore Tosti <saltosti@gmail.com>
2019-01-23 20:09:35 -0800

    Cleaned up index javascript

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,34 +1,3 @@
-$.socket = new Object()
-
-$(document).ready(function(){
-    $.socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-});
-
-$(document).ready(function(){
-    $.socket.on('toggle-response', function(msg) {
-        console.log(msg);
-    });
-});
-
-$(document).ready(function(){
-    $.socket.on('username-response', function(msg) {
-        console.log(msg);
-    });
-});
-
-
-$(function(){
-	$('#test-button').click(function() {
-        x = Number(window.prompt('x'))
-        y = Number(window.prompt('y'))
-        
-        $.socket.emit('toggle-event',{'room':'test','x':x,'y':y})
-        
-        // $.socket.emit('join', {'username': username,'room':'test'})
-        return false;
-    })
-})
-
 $(function(){
 	$('#start-button').click(function() {
         goToCreate()
