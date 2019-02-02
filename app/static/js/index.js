291844fdb248f4750498028b1401891ea5e760fa
Salvatore Tosti <saltosti@gmail.com>
2019-02-02 07:26:24 -0800

    Refactored rows to use flexbox rather than bootstrap form groups

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -6,27 +6,27 @@ $(function(){
 
 $(function(){
 	$('#join-button').click(function() {
-        if( $('#form').is(":valid") ){
-            boardName = $('#name').val()
-            $.ajax({
-                url: '/activeBoard/' + boardName,
-                type: 'GET',
-                success: function(response) {
-        			response = JSON.parse(response)
-                    isActiveBoard = response['active']
-                    console.log(isActiveBoard)
-                    if (isActiveBoard == 'true'){
-                        goToBoard(boardName)
-                    } else {
-                        $('#help-text').removeClass('invisible')
-                        $('#help-text').text('Room is not active.')
-                    }
-                }
-            })
-        } else {
-            $('#help-text').removeClass('invisible')   
-            $('#help-text').text('Please enter a valid room name. (no spaces)')   
+        boardName = $('#name').val()
+        if(!boardName.match("^[a-zA-Z0-9]*$")){
+            $('#help-text').removeClass('invisible')
+            $('#help-text').text('Please enter a valid room name. (no spaces)')
+            return
         }
+        
+        $.ajax({
+            url: '/activeBoard/' + boardName,
+            type: 'GET',
+            success: function(response) {
+        		response = JSON.parse(response)
+                isActiveBoard = response['active']
+                if (isActiveBoard == 'true'){
+                    goToBoard(boardName)
+                } else {
+                    $('#help-text').removeClass('invisible')
+                    $('#help-text').text('Room is not active.')
+                }
+            }
+        })
 	})
 })
 
